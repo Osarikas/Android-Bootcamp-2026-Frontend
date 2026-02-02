9d2af6cad003cb99f9611ac13d0b03f23767c9e8
Senik akkuratniy <senikdav@mail.ru>
2026-02-02 23:46:26 +0300

    FloatingActionButton

--- app/src/main/java/ru/sicampus/bootcamp2026/ui/MainScreen.kt
+++ app/src/main/java/ru/sicampus/bootcamp2026/ui/MainScreen.kt
@@ -2,9 +2,13 @@ package ru.sicampus.bootcamp2026.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.compose.rememberNavController
 import ru.sicampus.bootcamp2026.ui.bottom_navigation.BottomNavigation
 import ru.sicampus.bootcamp2026.ui.bottom_navigation.NavGraph
@@ -13,13 +17,22 @@ import ru.sicampus.bootcamp2026.ui.screen.meetings.search.ListScreen
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    Scaffold() { }
     Box(modifier = Modifier.fillMaxSize()) {
         NavGraph(navController)
         ListScreen() //Временно, чтобы отобразить список
-
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter
+            modifier = Modifier
+                .padding(bottom = 128.dp, end = 16.dp)
+                .align(Alignment.BottomEnd)
+                .zIndex(1f)
+        ) {
+            FloatingActionButton (navController)
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .zIndex(1f)
         ) {
             BottomNavigation(navController)
         }
